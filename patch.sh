178b3d401b92b870466da408d79abdb491b4f918
Jiab77 <jiab77@pm.me>
2023-07-09 19:17:58 +0200

    🔒️ Fixed invalid SRI

--- patch.sh
+++ patch.sh
@@ -35,7 +35,7 @@ LINE_TO_PATCH="</body>"
 LINE_TO_PATCH_POS=$(grep -n "$LINE_TO_PATCH" "$FILE_TO_PATCH" 2>/dev/null | awk '{ print $1 }' | sed -e 's/://')
 PATCH_CONTENT="\n\t<script src=\"/include/darkmode.js\" async defer></script>\n\t${LINE_TO_PATCH}"
 SRI_FILE="$BASE_DIR/patch.js.sri"
-SRI_STRING="eSJcaNtB94LcmGUFGXpOlKJt6lnWMDK4QTlkMIiNh8NfF22rcPz1gq5ewmynAgzi7c2c1xFBMQ9GfwN4Et1ilA=="
+SRI_STRING="gLKi5lBwmTl+pOPUbhO/CqdfPI4Kvot9Agp/H+bZmz9CRM3uxxXXYJfwUcTrSkb/mHXjaCT0EcoRUqVRc2uh4w=="
 PATCH_CONTENT_SRI="\n\t<script src=\"/include/darkmode.js\" integrity=\"sha512-${SRI_STRING}\" crossorigin=\"anonymous\" async defer></script>\n\t${LINE_TO_PATCH}"
 
 # Functions
